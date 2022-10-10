78379c2a0452d0b92738afef3d7b44f2540c3dfb
Lochan7JK <lochansingoria@gmail.com>
2022-10-10 23:17:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function loadCoupon(){
+    document.getElementById(`coupon`).style.visibility =`visible`
+    document.getElementById(`main`).style.opacity = `.5`
+}
+
+function closeCoupon(){
+    document.getElementById(`coupon`).style.visibility =`hidden`
+    document.getElementById(`main`).style.opacity = `1`
+}
