cc5a3e852babc948bc092e47a89bb1d6cfc248c6
Lochan7JK <lochansingoria@gmail.com>
2022-10-16 23:27:56 +0530

    lightdarkMode

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ function closeCoupon(){
     document.getElementById(`coupon`).style.visibility =`hidden`
     document.getElementById(`main`).style.opacity = `1`
 }
+
+
+function changeMode(){
+    let mybody = document.body;
+    mybody.classList.toggle(`mydark`)
+}
